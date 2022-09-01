3a628ec5f4982dd18eabac579bbb55d7fa6ba0ad
kingntop <kingntop@naver.com>
2022-09-01 16:30:59 +0900

    1

--- tests/test-1.spec.ts
+++ tests/test-1.spec.ts
@@ -13,9 +13,9 @@ test('test', async ({ page }) => {
   await page.locator('form[name="searchItem"] div:has-text("상품명 상품명 상품번호 공급사ID 공급사닉네임")').first().click();
 
   // Fill input[name="sw"]
-  await page.locator('input[name="sw"]').fill('');
+  await page.locator('input[name="sw"]').fill('ahslxj');
 
-  // Press Enterahsl
+  // Press Enter
   await page.locator('input[name="sw"]').press('Enter');
   await expect(page).toHaveURL('https://domeggook.com/main/item/itemList.php?sfc=ttl&sf=ttl&sw=ahslxj');
 
