9bd34a03fc7b37d44bbbe7ac11a2fae4a1013d42
kingntop <kingntop@naver.com>
2022-09-01 16:54:09 +0900

    11

--- tests/projects.spec.ts
+++ tests/projects.spec.ts
@@ -55,7 +55,7 @@ test('Projects Test', async ({}, expect) => {
     const context = await browser.newContext({
       recordVideo: {
         dir: dirVideo,
-        size: { width: 640, height: 640 },
+        size: { width: 640, height: 400 },
       }
     });
     await context.tracing.start({
